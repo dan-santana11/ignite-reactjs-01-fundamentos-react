7be7ab7b83713c8debc61e8909ff27c1010a214a
dan-santana11 <santanadaniel228@gmail.com>
2024-06-11 18:55:01 -0300

    Adding Component Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { Header } from './components/Header';
 import { Post } from './Post'
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-
-      <Post
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post
         author="Diego Fernandes"
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
       />
@@ -16,6 +21,8 @@ export function App() {
         author="Gabriel Buzzi"
         content="Um novo post muito legal"
       />
-    </div>
+        </main>
+      </div>
+    </div>  
   )
 }
