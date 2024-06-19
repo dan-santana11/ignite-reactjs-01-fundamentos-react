3ef24fc93e452f19e402dafa846d057e3c2bd956
dan-santana11 <santanadaniel228@gmail.com>
2024-06-19 10:15:35 -0300

    updating the avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react';
 
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar(options) {
     return (
@@ -9,9 +10,7 @@ export function Sidebar(options) {
            className={styles.cover} 
            src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=80&w=500&40=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
             <div className={styles.profile}>
-                <img
-                className={styles.avatar}
-                src="https://github.com/dan-santana11.png"/>
+            <Avatar src="https://github.com/dan-santana11.png"/>
             <strong>Daniel Santana</strong>
             <span>Web Developer</span>
             </div>
