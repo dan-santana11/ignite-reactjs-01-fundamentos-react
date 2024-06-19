967078c4382569a148865adc5e68bcded59a1a0e
dan-santana11 <santanadaniel228@gmail.com>
2024-06-19 09:44:11 -0300

    Comment Box

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,34 +1,35 @@
-import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 
-export function Comment(comment) {
-    return (
-        <div className={styles.comment}>
-            <img src="https://github.com/dan-santana11.png" alt=""/>
+import { ThumbsUp, Trash } from 'phosphor-react';
 
-            <div className={styles.commentBox}>
-                <div  className={styles.commentContent}>
-                    <header>
-                        <div className={styles.authorAndTime}>
-                            <strong>Daniel Santana</strong>
-                            <time title="11 de Maio ás 08:13h" dateTime="2024-06-13 08:13:30">Cerca de 1h átras</time>
-                        </div>
-                        <footer>
-                            <button>
-                                <Trash size={20}/>
-                            </button>
-                        </footer>
-                    </header>
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/dan-santana11.png" alt="" />
 
-                    <p>Muito bom Devon, parabéns!! 👏👏</p>
-                </div>
-                <footer>
-                    <button>
-                        <ThumbsUp />
-                        Aplaudir <span>20</span>
-                    </button>
-                </footer>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Daniel Santana</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
             </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
         </div>
-    );
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
 }
