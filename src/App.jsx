c7dead1138a2a75a8012f5a05d556a17792cf5ce
dan-santana11 <santanadaniel228@gmail.com>
2024-06-27 10:51:19 -0300

    properties of the post

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const posts = [
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
-    publishedAt: new Date('2022-06-27 20:00:00'),
+    publishedAt: new Date('2024-06-27 20:00:00'),
   },
   {
     id: 2,
