d73fc6f285b9ab6918b06e082c4bfa79952823c2
dan-santana11 <santanadaniel228@gmail.com>
2024-07-17 09:12:38 -0300

    Insert comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,9 +9,9 @@ import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
   const [comments, setComments] = useState([
-    1,
-    2,
-  ]);
+    'Post muito bacana, hein?!'
+  ])
+  const [newCommentText, setNewCommentText] = useState('')
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
@@ -25,9 +25,13 @@ export function Post({ author, publishedAt, content }) {
   function handleCrateNewComment() {
     event.preventDefault()
 
-    setComments([...comments, comments.length + 1]);
+    setComments([...comments, newCommentText]);  
+    setNewCommentText('');
   }
 
+  function handleNewCommentChange(event) {
+    setNewCommentText(event.target.value);
+  }
   return (
     <article className={styles.post}>
       <header>
@@ -58,7 +62,10 @@ export function Post({ author, publishedAt, content }) {
         <strong>Deixe seu feedback</strong>
 
         <textarea
+        name='comment'
           placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -68,7 +75,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment />
+          return <Comment content={comment}/>
         })}
       </div>
     </article>
